2b4b194e416394bddc940c893010cb403e788061
BuildTools <krutikovasjustinas@gmail.com>
2024-10-22 11:41:19 +0300

    sutvarkyta reprezentacine logika

--- JavaScript/2024-10-22/teorija/index.js
+++ JavaScript/2024-10-22/teorija/index.js
@@ -1,45 +1,14 @@
-// Tokios funkcijos kurios gali laukti.
-
-// suzinotiBtcKainas();
-// suzinotiBtcKainasLaukiantKiekvieno();
 console.log("komanda1");
-// kreipimasisAsinchroniskai();
-kreipimasis();
+kreipimasis(); //good
+// kreipimasisAsinchroniskai(); //good
+// kreipimasisIVienaApi(); //good
+// kreipimasisIKelisApi(); //bad
 console.log("komanda2");
 console.log("komanda3");
 console.log("komanda4");
 
-async function suzinotiBtcKainas() {
-	//geras pvz
-	const coins = ["BTCUSDT", "ETHUSDT", "DOTUSDT", "ADAUSDT"];
-	const urls = coins.map(
-		(coin) =>
-			`https://api.binance.com/api/v3/klines?symbol=${coin}&interval=1h&limit=1`
-	);
-	const promises = urls.map((url) => fetch(url));
-	const promiseResponses = await Promise.all(promises);
-	const data = await Promise.all(promiseResponses.map((resp) => resp.json()));
-	console.log(data);
-	for (const index in data) {
-		console.log(`Kriptovaliutos ${coins[index]} kaina: ${data[index][0][4]}`);
-	}
-}
-async function suzinotiBtcKainasLaukiantKiekvieno() {
-	//pvz kaip nereikia daryti
-	const coins = ["BTCUSDT", "ETHUSDT", "DOTUSDT", "ADAUSDT"];
-	const urls = coins.map(
-		(coin) =>
-			`https://api.binance.com/api/v3/klines?symbol=${coin}&interval=1h&limit=1`
-	);
-	for (const url of urls) {
-		const promise = await fetch(url);
-		const resp = await promise.json();
-		console.log(resp);
-	}
-}
-
-//
 function kreipimasis() {
+	// Šios funkcijos fetch'ai neblokuoja vienas kito, bendra trukme - ~3sek.
 	fetch("https://in3.dev/knygoss/") //404 not found
 		.then((resp) => {
 			console.log(resp);
@@ -49,6 +18,7 @@ function kreipimasis() {
 			console.log(data);
 		})
 		.catch((err) => {
+			//kadangi nuoroda bloga - vykdys .catch'e esancius veiksmus
 			console.log(err);
 			alert("Iskrito klaida kreipiantis į serverį");
 		});
@@ -62,44 +32,34 @@ function kreipimasis() {
 
 async function kreipimasisAsinchroniskai() {
 	//geras pvz
-	console.log("Asinchroninės funkcijos pradžia");
-	// const promise1 = await fetch("https://in3.dev/knygos/"); //2s
-	// const data1 = await promise1.json(); //100ms
-	// console.log(data1);
-	// const promise2 = await fetch("https://in3.dev/knygos/types/"); //2s
-	// const data2 = await promise2.json(); //100ms
-	// console.log(data2);
+	// Šios funkcijos fetch'ai neblokuoja vienas kito, bendra trukme - ~3sek.
 	const promises = await Promise.all([
 		fetch("https://in3.dev/knygos/"),
 		fetch("https://in3.dev/knygos/types/"),
 	]);
 	const duomenys = await Promise.all(promises.map((promise) => promise.json())); //140ms
-	// for (const promise of promises) {
-	// 	const data = await promise.json(); //100ms
-	// 	duomenys.push(data);
-	// } //500ms
+
 	console.log(duomenys);
 }
 
 async function kreipimasisIVienaApi() {
+	// Geras pvz
 	try {
 		const promise = await fetch("https://in3.dev/knygos/");
 		const resp = await promise.json();
+		//Jei atsakymas sekmingas:
 		console.log(resp);
 	} catch (err) {
+		//jei iskrenta klaida
 		alert("Iskrito klaida kreipiantis į serverį");
 	}
 }
 
-fetch("https://in3.dev/knygoss/") //404 not found
-	.then((resp) => {
-		console.log(resp);
-		return resp.json();
-	})
-	.then((data) => {
-		console.log(data);
-	})
-	.catch((err) => {
-		console.log(err);
-		alert("Iskrito klaida kreipiantis į serverį");
-	});
+async function kreipimasisIKelisApi() {
+	// Šios funkcijos fetchai atliekami paeiliui - pirma gaunamas 1 užklausos atsakymas,
+	// tada vykdoma antra užklausa
+	const promise1 = await fetch("https://in3.dev/knygos/");
+	const resp1 = await promise1.json();
+	const promise2 = await fetch("https://in3.dev/knygos/types");
+	const resp2 = await promise2.json();
+}
